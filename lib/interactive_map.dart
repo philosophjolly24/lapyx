b12267856d6296e75f34d04d780ee5cd33cb9e84
SunkenInTime <shawnadedeji@gmail.com>
2025-02-06 15:14:17 -0500

    Added mock data for strategy manager view

--- lib/interactive_map.dart
+++ lib/interactive_map.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
---
+++ lib/strategy_manager.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:icarus/strategy_view.dart';
+
+class StrategyManager extends ConsumerWidget {
+  const StrategyManager({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return GridView.builder(
+      itemCount: 10,
+      padding: const EdgeInsets.all(16),
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 268,
+        crossAxisSpacing: 16,
+        mainAxisSpacing: 16,
+        childAspectRatio: 1,
+      ),
+      itemBuilder: (context, index) {
+        if (index == 0) {
+          return InkWell(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Color(0xFF151515),
+                borderRadius: BorderRadius.all(Radius.circular(24)),
+              ),
+              child: const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.add),
+                  SizedBox(height: 20),
+                  Text("New Strategy")
+                ],
+              ),
+            ),
+            onTap: () {},
+          );
+        }
+        return InkWell(
+          child: Container(
+            decoration: const BoxDecoration(
+              color: Color(0xFF151515),
+              borderRadius: BorderRadius.all(Radius.circular(24)),
+            ),
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 193,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      borderRadius:
+                          BorderRadius.vertical(top: Radius.circular(24)),
+                      color: Colors.red,
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text("Strategy 1"),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 8, bottom: 8),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text("Ascent"),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          onTap: () {},
+        );
+      },
+    );
+  }
+}
