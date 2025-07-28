dc8b4846b15adcd83c1742560dca872a2dc9311c
Dara Adedeji <76637177+SunkenInTime@users.noreply.github.com>
2025-07-27 23:17:45 -0400

    1.5.0 release-We are finally launching!!

--- lib/const/settings.dart
+++ lib/const/settings.dart
@@ -29,5 +29,5 @@ class Settings {
   static final Uri dicordLink = Uri.parse("https://discord.gg/PN2uKwCqYB");
 
   static const Duration autoSaveOffset = Duration(seconds: 15);
-  static const int versionNumber = 3;
+  static const int versionNumber = 5;
 }
